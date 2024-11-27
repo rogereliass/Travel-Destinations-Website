37353cfd8b35af7ef7850b162f129dfdb482d8e5
rogereliass <roger.elias669@gmail.com>
2024-11-27 19:53:39 +0200

    testing

--- myApp/app.js
+++ myApp/app.js
@@ -1,14 +1,12 @@
 
 var express = require('express');
 var path = require('path');
-
-
+var fs = require('fs');
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -20,6 +18,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/',function(req,res){
   res.render('rome')
 });
+
 
 
 
